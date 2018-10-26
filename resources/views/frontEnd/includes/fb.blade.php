a0c279bec1ab8665b104fbfeabc2749e0624ca5c
Md. Mayeen Uddin <mayeennbd@gmail.com>
2018-10-26 14:56:21 +0600

    Update Manything

---
+++ resources/views/frontEnd/includes/fb.blade.php
@@ -0,0 +1,15 @@
+<div class="footer">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 footer-w3ls1">
+			<ul class="bottom-links">
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			</ul>
+		</div>
+            <div class="col-lg-6 col-md-6 footer-w3ls2">
+			 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FConcrete-Tours-Travels-1966937990229395%2F&tabs=timeline&width=300px&height=150px&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId=161252951286869" width="300px" height="150px" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+		</div>
+	</div>
+</div>
---
+++ resources/views/frontEnd/includes/gallery1.blade.php
@@ -0,0 +1,325 @@
+<section class="work" id="work">
+	<h3 class="text-center">Our Work</h3>
+	<p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	<div class="gallery-grids">
+		<div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
+			<ul id="myTab" class="nav nav-tabs" role="tablist">
+				<li role="presentation" class="active"><a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">All</a></li>
+				<li role="presentation"><a href="#hotel" role="tab" id="hotel-tab" data-toggle="tab" aria-controls="hotel">Hotels</a></li>
+				<li role="presentation"><a href="#restaurant" role="tab" id="restaurant-tab" data-toggle="tab" aria-controls="restaurant">restaurants</a></li>
+				<li role="presentation"><a href="#cruise" role="tab" id="cruise-tab" data-toggle="tab" aria-controls="cruise">Cruising</a></li>
+				<li role="presentation"><a href="#sea" role="tab" id="sea-tab" data-toggle="tab" aria-controls="sea">Offshore</a></li>
+			</ul>
+			<div id="myTabContent" class="tab-content">
+				<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work1.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work1.jpg" class="img-responsive" alt="" />
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work2.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work2.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work3.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work3.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work4.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work4.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>restaurants</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work5.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work5.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work6.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work6.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work7.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work7.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work8.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work8.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work9.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work9.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work10.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work10.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work11.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work11.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>restaurants</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work12.jpg" class="lightninBox" data-lb-group="1">
+								<img src="{{asset('public/frontEnd/')}}/images/work12.jpg" class="img-responsive" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div role="tabpanel" class="tab-pane fade" id="hotel" aria-labelledby="hotel-tab">
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work1.jpg" class="lightninBox" data-lb-group="2">
+								<img src="{{asset('public/frontEnd/')}}/images/work1.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work3.jpg" class="lightninBox" data-lb-group="2">
+								<img src="{{asset('public/frontEnd/')}}/images/work3.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work9.jpg" class="lightninBox" data-lb-group="2">
+								<img src="{{asset('public/frontEnd/')}}/images/work9.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work10.jpg" class="lightninBox" data-lb-group="2">
+								<img src="{{asset('public/frontEnd/')}}/images/work10.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work12.jpg" class="lightninBox" data-lb-group="2">
+								<img src="{{asset('public/frontEnd/')}}/images/work12.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>	
+				</div>
+				<div role="tabpanel" class="tab-pane fade" id="restaurant" aria-labelledby="restaurant-tab">
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work4.jpg" class="lightninBox" data-lb-group="3">
+								<img src="{{asset('public/frontEnd/')}}/images/work4.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>restaurants</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work11.jpg" class="lightninBox" data-lb-group="3">
+								<img src="{{asset('public/frontEnd/')}}/images/work11.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>restaurants</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work12.jpg" class="lightninBox" data-lb-group="3">
+								<img src="{{asset('public/frontEnd/')}}/images/work12.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>	
+				</div>
+				<div role="tabpanel" class="tab-pane fade" id="cruise" aria-labelledby="cruise-tab">
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work2.jpg" class="lightninBox" data-lb-group="4">
+								<img src="{{asset('public/frontEnd/')}}/images/work2.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work7.jpg" class="lightninBox" data-lb-group="4">
+								<img src="{{asset('public/frontEnd/')}}/images/work7.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work8.jpg" class="lightninBox" data-lb-group="4">
+								<img src="{{asset('public/frontEnd/')}}/images/work8.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Cruising</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>	
+				</div>
+				<div role="tabpanel" class="tab-pane fade" id="sea" aria-labelledby="sea-tab">
+					<div class="tab_img">
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work1.jpg" class="lightninBox" data-lb-group="5">
+								<img src="{{asset('public/frontEnd/')}}/images/work1.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work3.jpg" class="lightninBox" data-lb-group="5">
+								<img src="{{asset('public/frontEnd/')}}/images/work3.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work5.jpg" class="lightninBox" data-lb-group="5">
+								<img src="{{asset('public/frontEnd/')}}/images/work5.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work6.jpg" class="lightninBox" data-lb-group="5">
+								<img src="{{asset('public/frontEnd/')}}/images/work6.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 portfolio-grids">
+							<a href="{{asset('public/frontEnd/')}}/images/work12.jpg" class="lightninBox" data-lb-group="5">
+								<img src="{{asset('public/frontEnd/')}}/images/work12.jpg" class="img-responsive zoom-img" alt=""/>
+								<div class="b-wrapper">
+									<i class="fa fa-search-plus" aria-hidden="true"></i>
+									<h5>Hotels & Ocean Scenery</h5>
+									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+								</div>
+							</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>	
+</section>
